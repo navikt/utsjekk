cbbea2fbb44d0be9d2311f87aa7001291c54af54
jobi <jorn.ola.birkeland@nav.no>
2023-03-23 13:11:20 +0100

    Legger til kafkacontainer i ApplicationLocal

--- dp-iverksett-api/src/test/kotlin/no/nav/dagpenger/iverksett/ApplicationLocal.kt
+++ dp-iverksett-api/src/test/kotlin/no/nav/dagpenger/iverksett/ApplicationLocal.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.iverksett
 
 import no.nav.dagpenger.iverksett.infrastruktur.configuration.ApplicationConfig
 import no.nav.dagpenger.iverksett.infrastruktur.database.DbContainerInitializer
+import no.nav.dagpenger.iverksett.infrastruktur.kafka.KafkaContainerInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
@@ -11,7 +12,7 @@ class ApplicationLocal
 
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationConfig::class.java)
-        .initializers(DbContainerInitializer())
+        .initializers(DbContainerInitializer(), KafkaContainerInitializer())
         .profiles(
             "local",
             "mock-integrasjoner",
