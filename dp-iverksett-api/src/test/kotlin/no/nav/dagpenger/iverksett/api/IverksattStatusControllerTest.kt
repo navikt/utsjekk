bb4dd99427b7b0e747ded13fd533ce528b00471c
Martine Enger <marenger@student.matnat.uio.no>
2023-05-03 10:57:04 +0200

    Test responskoder fra IverksattStatusController

---
+++ dp-iverksett-api/src/test/kotlin/no/nav/dagpenger/iverksett/api/IverksattStatusControllerTest.kt
@@ -0,0 +1,35 @@
+package no.nav.dagpenger.iverksett.api
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.dagpenger.iverksett.util.vedtaksdetaljerDagpenger
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+
+
+class IverksattStatusControllerTest {
+
+    private val vedtakStatusServiceMock: VedtakStatusService = mockk()
+    private val iverksattStatusController = IverksattStatusController(vedtakStatusServiceMock)
+
+    @Test
+    fun `skal svare med 404 når person ikke har noen vedtak`() {
+        every { vedtakStatusServiceMock.getVedtakStatus(any()) } returns null
+
+        val response = iverksattStatusController.hentStatusForPerson("12345678910")
+
+        assertEquals(HttpStatus.NOT_FOUND, response.statusCode)
+    }
+
+    @Test
+    fun `skal svare med 200 når person har iverksatt vedtak`() {
+        val vedtaksdetaljerDagpenger = vedtaksdetaljerDagpenger()
+        every { vedtakStatusServiceMock.getVedtakStatus(any()) } returns vedtaksdetaljerDagpenger
+
+        val response = iverksattStatusController.hentStatusForPerson("12345678910")
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(vedtaksdetaljerDagpenger, response.body)
+    }
+}
