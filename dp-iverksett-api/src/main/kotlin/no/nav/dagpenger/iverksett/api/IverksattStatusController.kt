e49675a75d70f2f5d3c33bf51e0b900f55c2815b
Martine Enger <marenger@student.matnat.uio.no>
2023-05-03 13:27:29 +0200

    Autentisering på IverksattStatus-endepunkt

--- dp-iverksett-api/src/main/kotlin/no/nav/dagpenger/iverksett/api/IverksattStatusController.kt
+++ dp-iverksett-api/src/main/kotlin/no/nav/dagpenger/iverksett/api/IverksattStatusController.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.iverksett.api
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import no.nav.dagpenger.iverksett.api.domene.VedtaksdetaljerDagpenger
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController
     path = ["/api/vedtakstatus"],
     produces = [MediaType.APPLICATION_JSON_VALUE],
 )
+@ProtectedWithClaims(issuer = "azuread")
 class IverksattStatusController(
     private val vedtakStatusService: VedtakStatusService,
 ) {
