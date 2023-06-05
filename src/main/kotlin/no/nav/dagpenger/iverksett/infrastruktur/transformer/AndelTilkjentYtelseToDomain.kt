eef9e95985b7724aacd7aed2e582d3be8b87669b
jobi <jorn.ola.birkeland@nav.no>
2023-06-05 17:11:17 +0200

    Tar i bruk ny versjon av kontrakter med nye felter i utbetalinger

--- src/main/kotlin/no/nav/dagpenger/iverksett/infrastruktur/transformer/AndelTilkjentYtelseToDomain.kt
+++ src/main/kotlin/no/nav/dagpenger/iverksett/infrastruktur/transformer/AndelTilkjentYtelseToDomain.kt
@@ -6,9 +6,9 @@ import no.nav.dagpenger.kontrakter.iverksett.UtbetalingDto
 
 fun UtbetalingDto.toDomain(): AndelTilkjentYtelse {
     return AndelTilkjentYtelse(
-        beløp = this.beløp,
+        beløp = this.beløp ?: this.belopPerDag ?: throw IllegalStateException("Mangler feltet belopPerDag"),
         periode = Datoperiode(this.fraOgMedDato, this.tilOgMedDato),
-        stønadstype = this.stønadstype,
+        stønadstype = this.stønadstype ?: stonadstype,
         ferietillegg = this.ferietillegg,
     )
 }
