9b31a87fe2df704b194168de5e8945e42cd7a80c
Martine Enger <marenger@student.matnat.uio.no>
2023-06-22 13:49:37 +0200

    Tester på validering av tilstand

---
+++ src/test/kotlin/no/nav/dagpenger/iverksett/api/IverksettingValidatorServiceTest.kt
@@ -0,0 +1,56 @@
+package no.nav.dagpenger.iverksett.api
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.dagpenger.iverksett.api.tilstand.IverksettResultatService
+import no.nav.dagpenger.iverksett.infrastruktur.featuretoggle.FeatureToggleService
+import no.nav.dagpenger.iverksett.lagIverksettData
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+
+class IverksettingValidatorServiceTest {
+
+    private val iverksettResultatServiceMock = mockk<IverksettResultatService>()
+    private val iverksettingServiceMock = mockk<IverksettingService>()
+    private val featureToggleServiceMock = mockk<FeatureToggleService>()
+    private lateinit var iverksettingValidatorService: IverksettingValidatorService
+
+    @BeforeEach
+    fun setup() {
+        iverksettingValidatorService = IverksettingValidatorService(
+            iverksettResultatServiceMock,
+            iverksettingServiceMock,
+            featureToggleServiceMock
+        )
+    }
+
+    @Test
+    fun `skal få BAD_REQUEST når forrige iverksetting er knyttet til en annen sak`() {
+        val forrigeIverksetting = lagIverksettData()
+        val nåværendeIverksetting = lagIverksettData(
+            forrigeBehandlingId = forrigeIverksetting.behandling.behandlingId
+        )
+        every { iverksettingServiceMock.hentForrigeIverksett(nåværendeIverksetting) } returns forrigeIverksetting
+
+        assertApiFeil(HttpStatus.BAD_REQUEST) {
+            iverksettingValidatorService.validerAtIverksettingErForSammeSakOgPersonSomForrige(nåværendeIverksetting)
+        }
+    }
+
+    @Test
+    fun `skal få BAD_REQUEST når forrige iverksetting er knyttet til en annen person`() {
+        val forrigeIverksetting = lagIverksettData()
+        val iverksettingTmp = lagIverksettData()
+        val nåværendeIverksetting = iverksettingTmp.copy(
+            fagsak = forrigeIverksetting.fagsak,
+            forrigeIverksetting = forrigeIverksetting,
+            søker = iverksettingTmp.søker.copy(personIdent = "12345678911")
+        )
+        every { iverksettingServiceMock.hentForrigeIverksett(nåværendeIverksetting) } returns forrigeIverksetting
+
+        assertApiFeil(HttpStatus.BAD_REQUEST) {
+            iverksettingValidatorService.validerAtIverksettingErForSammeSakOgPersonSomForrige(nåværendeIverksetting)
+        }
+    }
+}
