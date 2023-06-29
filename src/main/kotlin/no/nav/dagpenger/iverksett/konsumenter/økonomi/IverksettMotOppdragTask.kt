730878628d36eaa088bc3a20c4d010e7ed6f9198
jobi <jorn.ola.birkeland@nav.no>
2023-06-29 09:58:30 +0200

    Jobber videre med å ta i bruk ny utbetalingsgenerator

--- src/main/kotlin/no/nav/dagpenger/iverksett/konsumenter/økonomi/IverksettMotOppdragTask.kt
+++ src/main/kotlin/no/nav/dagpenger/iverksett/konsumenter/økonomi/IverksettMotOppdragTask.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.iverksett.konsumenter.økonomi
 
 import no.nav.dagpenger.iverksett.api.IverksettingService
+import no.nav.dagpenger.iverksett.api.domene.AndelTilkjentYtelse
 import no.nav.dagpenger.iverksett.api.domene.erKonsistentMed
 import no.nav.dagpenger.iverksett.api.tilstand.IverksettResultatService
 import no.nav.dagpenger.iverksett.konsumenter.opprettNesteTask
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service
 import java.util.UUID
 import no.nav.dagpenger.iverksett.api.domene.IverksettDagpenger
 import no.nav.dagpenger.iverksett.api.domene.IverksettResultat
+import no.nav.dagpenger.iverksett.api.domene.TilkjentYtelse
 import no.nav.dagpenger.iverksett.api.domene.behandlingId
 import no.nav.dagpenger.iverksett.api.domene.lagAndelData
 import no.nav.dagpenger.iverksett.api.domene.personIdent
@@ -22,6 +24,7 @@ import no.nav.dagpenger.iverksett.api.domene.sakId
 import no.nav.dagpenger.iverksett.api.domene.tilAndelData
 import no.nav.dagpenger.iverksett.konsumenter.økonomi.utbetalingsoppdrag.ny.Utbetalingsgenerator
 import no.nav.dagpenger.iverksett.konsumenter.økonomi.utbetalingsoppdrag.ny.domene.Behandlingsinformasjon
+import no.nav.dagpenger.iverksett.konsumenter.økonomi.utbetalingsoppdrag.ny.domene.StønadTypeOgFerietillegg
 
 @Service
 @TaskStepBeskrivelse(
@@ -54,6 +57,7 @@ class IverksettMotOppdragTask(
         lagOgSendUtbetalingsoppdragOgOppdaterTilkjentYtelse(iverksett, forrigeIverksettResultat, behandlingId)
     }
 
+
     private fun nyLagOgSendUtbetalingsoppdragOgOppdaterTilkjentYtelse(
         iverksett: IverksettDagpenger,
         forrigeIverksettResultat: IverksettResultat?,
@@ -80,10 +84,73 @@ class IverksettMotOppdragTask(
             sisteAndelPerKjede = sisteAndelPerKjede
         )
 
+        val nyeAndelerMedPeriodeId = iverksett.vedtak.tilkjentYtelse?.andelerTilkjentYtelse?.map {
+            val andelData = it.tilAndelData()
+            val andelDataMedPeriodeId = beregnetUtbetalingsoppdrag.andeler.find { andelData.id == it.id }
+                ?: throw IllegalStateException("Fant ikke andel med id ${andelData.id}")
+
+            it.copy(
+                periodeId = andelDataMedPeriodeId.periodeId,
+                forrigePeriodeId = andelDataMedPeriodeId.forrigePeriodeId
+            )
+        } ?: emptyList()
+
+        val nyTilkjentYtelse = iverksett.vedtak.tilkjentYtelse?.copy(
+            andelerTilkjentYtelse = nyeAndelerMedPeriodeId,
+        )
+
+        if(nyTilkjentYtelse!=null) {
+            val nyTilkjentYtelseMedSisteAndelIKjede = lagTilkjentYtelseMedSisteAndelPerKjede(nyTilkjentYtelse)
+            iverksettResultatService.oppdaterTilkjentYtelseForUtbetaling(
+                behandlingId = behandlingId,
+                tilkjentYtelseForUtbetaling = nyTilkjentYtelseMedSisteAndelIKjede
+            )
+            
+        }
+
+
         // TODO det gjenstår å finne nye sisteAndelPerKjede basert på resultatet, oppdatere tilkjentytelse og iverksette.
+        val sisteAndelIKjeder: Map<StønadTypeOgFerietillegg, AndelTilkjentYtelse> = beregnetUtbetalingsoppdrag.andeler.
+
         // Vi tror sisteAndelPerKjede kan løses ved en dobbel sortering på periodeId først, så tom.
 
     }
+
+    private fun lagTilkjentYtelseMedSisteAndelPerKjede(tilkjentYtelse: TilkjentYtelse): TilkjentYtelse {
+        val beregnetSisteAndePerKjede = tilkjentYtelse.andelerTilkjentYtelse.groupBy {
+            StønadTypeOgFerietillegg(it.stønadstype, it.ferietillegg)
+        }.mapValues {
+            it.value.maxBy { it.periodeId!! }
+        }
+
+        val forrigeSisteAndelPerKjede = tilkjentYtelse.sisteAndelPerKjede
+
+        val nySisteAndelerPerKjede: Map<StønadTypeOgFerietillegg, AndelTilkjentYtelse> =
+            finnSisteAndelPerKjede(beregnetSisteAndePerKjede, forrigeSisteAndelPerKjede)
+
+        return tilkjentYtelse.copy(
+            sisteAndelPerKjede = nySisteAndelerPerKjede
+        )
+    }
+
+    /**
+     * Finner riktig siste andel per kjede av andeler
+     * Key er definert av stønadstype + ferietillegg
+     * Funksjonen lager en map med kjedeId som key og en liste med de to andelene fra hver map
+     * Deretter finner vi hvilke av de to vi skal bruke, Regelen er
+     * 1. Bruk den med største periodeId
+     * 2. Hvis periodeIdene er like, bruk den med størst til-og-med-dato
+     */
+    private fun finnSisteAndelPerKjede(
+        nySisteAndePerKjede: Map<StønadTypeOgFerietillegg, AndelTilkjentYtelse>,
+        forrigeSisteAndelPerKjede: Map<StønadTypeOgFerietillegg, AndelTilkjentYtelse>
+    ) = (nySisteAndePerKjede.asSequence() + forrigeSisteAndelPerKjede.asSequence())
+        .groupBy({ it.key }, { it.value })
+        .mapValues {
+            it.value.sortedWith(compareBy<AndelTilkjentYtelse> { it.periodeId }.thenBy { it.periode.tom })
+                .last()
+        }
+
 
     private fun lagOgSendUtbetalingsoppdragOgOppdaterTilkjentYtelse(
         iverksett: IverksettDagpenger,
