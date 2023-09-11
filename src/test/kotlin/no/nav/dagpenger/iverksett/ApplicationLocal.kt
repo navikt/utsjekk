2b6c375313b6795ed87eef1454277054f5d7720b
Stephen Ramthun <stephenramthun@gmail.com>
2023-09-11 12:41:18 +0200

    Fjern initialisering av kafka
    Co-authored-by: Martine Enger <marenger@student.matnat.uio.no>

--- src/test/kotlin/no/nav/dagpenger/iverksett/ApplicationLocal.kt
+++ src/test/kotlin/no/nav/dagpenger/iverksett/ApplicationLocal.kt
@@ -2,7 +2,6 @@ package no.nav.dagpenger.iverksett
 
 import no.nav.dagpenger.iverksett.infrastruktur.configuration.ApplicationConfig
 import no.nav.dagpenger.iverksett.infrastruktur.database.DbContainerInitializer
-import no.nav.dagpenger.iverksett.infrastruktur.kafka.KafkaContainerInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
@@ -12,7 +11,7 @@ class ApplicationLocal
 
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationConfig::class.java)
-        .initializers(DbContainerInitializer(), KafkaContainerInitializer())
+        .initializers(DbContainerInitializer())
         .profiles(
             "local",
             "mock-integrasjoner",
