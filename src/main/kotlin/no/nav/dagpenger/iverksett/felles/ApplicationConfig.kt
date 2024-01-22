f6dc77f30b3462b39251a69033f8dffa912edeff
Martine Enger <marenger@student.matnat.uio.no>
2024-01-22 16:59:53 +0100

    Legg tilbake RetryOAuth2HttpClient - klarer visst ikke hente token for dp-oppdrag uten den 🧐

--- src/main/kotlin/no/nav/dagpenger/iverksett/felles/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/dagpenger/iverksett/felles/ApplicationConfig.kt
@@ -3,9 +3,11 @@ package no.nav.dagpenger.iverksett.felles
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.dagpenger.iverksett.felles.http.ObjectMapperProvider
 import no.nav.dagpenger.iverksett.felles.oppdrag.konfig.RestTemplateAzure
+import no.nav.dagpenger.iverksett.felles.oppdrag.konfig.RetryOAuth2HttpClient
 import no.nav.familie.log.filter.LogFilter
 import no.nav.familie.log.filter.RequestTimeFilter
 import no.nav.familie.prosessering.config.ProsesseringInfoProvider
+import no.nav.security.token.support.client.core.http.OAuth2HttpClient
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
@@ -13,6 +15,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
@@ -21,6 +24,8 @@ import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan(
@@ -44,7 +49,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableOAuth2Client(cacheEnabled = true)
 @EnableScheduling
 class ApplicationConfig {
-
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @Bean
@@ -72,15 +76,31 @@ class ApplicationConfig {
     @Primary
     fun objectMapper() = ObjectMapperProvider.objectMapper
 
+    /**
+     * Overskrever OAuth2HttpClient som settes opp i token-support som ikke kan få med objectMapper fra felles
+     * pga .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
+     * og [OAuth2AccessTokenResponse] som burde settes med setters, då feltnavn heter noe annet enn feltet i json
+     */
+    @Bean
+    @Primary
+    fun oAuth2HttpClient(): OAuth2HttpClient {
+        return RetryOAuth2HttpClient(
+            RestTemplateBuilder()
+                .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
+                .setReadTimeout(Duration.of(2, ChronoUnit.SECONDS)),
+        )
+    }
+
     @Bean
     fun prosesseringInfoProvider(
         @Value("\${PROSESSERING_GRUPPE}") gruppe: String,
     ) = object : ProsesseringInfoProvider {
-        override fun hentBrukernavn(): String = try {
-            SpringTokenValidationContextHolder().tokenValidationContext.getClaims("azuread").getStringClaim("preferred_username")
-        } catch (e: Exception) {
-            "dp-iverksett"
-        }
+        override fun hentBrukernavn(): String =
+            try {
+                SpringTokenValidationContextHolder().tokenValidationContext.getClaims("azuread").getStringClaim("preferred_username")
+            } catch (e: Exception) {
+                "dp-iverksett"
+            }
 
         override fun harTilgang(): Boolean = grupper().contains(gruppe)
 
