5c2487469a560748a80825dac1b45fa4f50af572
Stephen Ramthun <stephenramthun@gmail.com>
2024-06-12 14:15:25 +0200

    Send videre feil ved kall til simulering
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>

--- src/main/kotlin/no/nav/utsjekk/simulering/api/SimuleringController.kt
+++ src/main/kotlin/no/nav/utsjekk/simulering/api/SimuleringController.kt
@@ -3,11 +3,13 @@ package no.nav.utsjekk.simulering.api
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.utsjekk.simulering.SimuleringService
 import no.nav.utsjekk.simulering.api.Mapper.tilInterntFormat
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
 
 @RestController
 @RequestMapping("/api/simulering")
@@ -16,8 +18,16 @@ class SimuleringController(private val simuleringService: SimuleringService) {
     @PostMapping
     fun hentSimulering(
         @RequestBody requestDto: SimuleringRequestDto,
-    ): ResponseEntity<SimuleringResponsDto> {
+    ) = try {
         val respons = simuleringService.hentSimuleringsresultatMedOppsummering(requestDto.tilInterntFormat())
-        return ResponseEntity.ok(respons)
+        ResponseEntity.ok(respons)
+    } catch (e: HttpClientErrorException) {
+        when (e.statusCode) {
+            HttpStatus.NOT_FOUND,
+            HttpStatus.BAD_REQUEST,
+            HttpStatus.SERVICE_UNAVAILABLE,
+            HttpStatus.CONFLICT -> ResponseEntity.status(e.statusCode).body(e.message)
+            else -> ResponseEntity.internalServerError()
+        }
     }
 }
